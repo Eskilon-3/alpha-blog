7280e83b2eaf7b146d8e5220cecf37c46075d4e9
Julio Ferreira <juliof0_@hotmail.com>
2020-11-11 17:19:10 -0300

    Adding new stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfuly signed up"
+            flash[:notice] = "Welcome to Book G #{@user.username}, you have successfuly signed up"
             redirect_to books_path
             else
             render 'new'
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
     def destroy
         @user.destroy
         session[:user_id] = nil if @user == current_user
-        flash[:notice] = "Account and all associated articles successfully deleted"
+        flash[:notice] = "Account and all associated books successfully deleted"
         redirect_to books_path
     end
 
